cb2bd9112439462043108b3185728b21d221ef7c
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-25 13:47:31 +0200

    initial commit

---
+++ src/test/java/formacion/back/Bs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package formacion.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
