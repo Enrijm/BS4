3ccea5c29937a347dae981e94daf8820999ae0f5
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-26 09:27:21 +0200

    application.yml

--- src/main/java/formacion/back/GET.java
+++ src/main/java/formacion/back/GET.java
@@ -1,7 +1,7 @@
 package formacion.back;
 
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
